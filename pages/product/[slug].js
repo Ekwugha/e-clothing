d720f9c2033e327df687b3ccdc63f7d890222730
Ekwugha <elloekwugha@gmail.com>
2023-01-06 12:52:07 +0100

    created shopping cart page & list of items in cart

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import  { useRouter } from 'next/router';
 import React, { useContext } from 'react';
 import Homepage from '../../components/Homepage';
 import data from '../../utils/data';
@@ -9,6 +9,8 @@ import { Store } from '../../utils/Store';
 export default function ProductScreen() {
   // define the state and dispatch
   const { state, dispatch } = useContext(Store);
+  // use it to redirect to another page
+  const router = useRouter();
   // we use this to get the product(name, slug e.t.c) from the url
   const { query } = useRouter();
   // then from the query we get slug where we can get the product
@@ -25,11 +27,13 @@ export default function ProductScreen() {
     const quantity = existItem ? existItem.quantity + 1 : 1;
     // This makes sure you don't add more than the amount of product in stock
     if (product.countInStock < quantity) {
-        alert('Sorry, Product is out of stock');
-        return;
+      alert('Sorry, Product is out of stock');
+      return;
     }
     // we use the dispatch inside the add to carthandler
     dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+    // redirect user to the cart page after clicking add to cart
+    router.push('/cart');
   };
 
   return (
--- utils/Store.js
+++ utils/Store.js
@@ -12,16 +12,25 @@ function reducer(state, action) {
     // if action.type is CART_ADD_ITEM, then we need to update the state and add new item to the cart
     case 'CART_ADD_ITEM': {
       const newItem = action.payload;
-    //   search the state for the newItem using find method on the cartItem in the cart object that is in the state. If the item with the slug has been found then existItem gets the item
+      //   search the state for the newItem using find method on the cartItem in the cart object that is in the state. If the item with the slug has been found then existItem gets the item
       const existItem = state.cart.cartItems.find(
         (item) => item.slug === newItem.slug
       );
-    // The map function checks each items in the cart item if they're equal to existitem & replace it with the new item cause it contains the new quantity of the items in the cart otherwise keeps the items as they are.
+      // The map function checks each items in the cart item if they're equal to existitem & replace it with the new item cause it contains the new quantity of the items in the cart otherwise keeps the items as they are.
       const cartItems = existItem
         ? state.cart.cartItems.map((item) =>
             item.name === existItem.name ? newItem : item
           )
         : [...state.cart.cartItems, newItem];
+      return { ...state, cart: { ...state.cart, cartItems } };
+    }
+
+    // another case to remove cart item
+    case 'CART_REMOVE_ITEM': {
+      const cartItems = state.cart.cartItems.filter(
+        (item) => item.slug !== action.payload.slug
+      );
+      // return previous state in the cart, keep previous cart and then pass the cartItems as a parameter   
       return { ...state, cart: { ...state.cart, cartItems } };
     }
     default:
